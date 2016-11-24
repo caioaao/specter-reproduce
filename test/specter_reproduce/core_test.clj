024e969fd49c1248379426c38482ca964e59e37c
Caio Oliveira <caio.oliveira@nubank.com.br>
2016-11-24 15:33:54 -0200

    cleaning up

--- test/specter_reproduce/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns specter-reproduce.core-test
-  (:require [clojure.test :refer :all]
-            [specter-reproduce.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
